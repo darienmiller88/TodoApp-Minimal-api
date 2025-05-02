d89658fe6317e43dafff1fe2a55559c420b40b25
unknown <darienmiller88@yahoo.com>
2025-05-02 12:33:57 -0400

    added getAllComplete and getAllIncomplete services

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using api.v1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//'AddSingleton' ensures that the TodoService instance is shared for all routes, and persists as long as the server is
+//running. NOTE: this is just temporary until I get database functionality up and running. Afterwards, I will switch
+//back to AddScoped.
 builder.Services.AddSingleton<ITodoService, TodoService>();
 
 var app = builder.Build();
--- api/v1/Services/TodoService/TodoService.cs
+++ api/v1/Services/TodoService/TodoService.cs
@@ -16,27 +16,54 @@ class TodoService : ITodoService{
         new Todo("Complete Todo list before day ends", 2)
     ];
 
-    // This method returns an array of Todo objects from the database eventually.
+    //GET: This method returns an array of Todo objects from the database eventually.
     public List<Todo> GetTodos(){
         return todos;
     }
 
+    //GET: Get a todo that with an id of 'id'
     public ServiceResult<Todo> GetTodoById(int id){
         Todo? todo = todos.FirstOrDefault(todo => todo.id == id);
 
+        //If there is no todo with an id of 'id' found in the list of todos, return 404 not found.
         if (todo == null) {
             return new ServiceResult<Todo>($"Todo with id: {id} not found.", 404, null);
         }
 
+        //otherwise return a 200 and the todo that was found.
         return new ServiceResult<Todo>("", 200, todo);
     }
 
-    // Function to add a todo to the array,  and eventually to the database.
+    //GET: Retrieves all todos that are completed.
+    public ServiceResult<List<Todo>> GetCompletedTodos(){
+        List<Todo> completedTodos = todos.FindAll(todo => todo.isComplete);
+
+        //If there are no completed todos, return a 404 to the client telling them such.
+        if (completedTodos.Count == 0){
+            return new ServiceResult<List<Todo>>("No todos are completed... yet.", 404, null);
+        }
+
+        return new ServiceResult<List<Todo>>("", 200, completedTodos);
+    }
+
+    //GET: Retrieves all todos that are completed.
+    public ServiceResult<List<Todo>> GetIncompletedTodos(){
+        List<Todo> completedTodos = todos.FindAll(todo => todo.isComplete);
+
+        //If there are no INCOMPLETE todos, return a 404 to the client telling them such.
+        if (completedTodos.Count == 0){
+            return new ServiceResult<List<Todo>>("No todos are incomplete.", 404, null);
+        }
+
+        return new ServiceResult<List<Todo>>("", 200, completedTodos);
+    }
+
+    //POST: Function to add a todo to the array,  and eventually to the database.
      public ServiceResult<Todo> AddTodo(Todo newTodo){
 
         //First, check to see if there is a todo with a duplicate ID in the list of todos
         if (todos.Any(todo => todo.id == newTodo.id)){
-            return new ServiceResult<Todo>($"Todo with id of {newTodo.id} already exists!", 409, null);
+            return new ServiceResult<Todo>($"Todo with id of \'{newTodo.id}\' already exists!", 409, null);
         }
 
         //Second, check to see if there is a todo with a duplicate name in the list of todos.
@@ -50,4 +77,6 @@ class TodoService : ITodoService{
         //Send the todo back with a successful code of 201.
         return new ServiceResult<Todo>("", 201, newTodo);
     }
+
+
 };
