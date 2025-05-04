6ccc78b1c93d39db485db8611b37c35f317bd30e
unknown <darienmiller88@yahoo.com>
2025-05-03 20:40:36 -0400

    feat: added all GET routes to TodoRoutes; will add the rest later

--- api/v1/Routes/TodoRoutes/TodoRoutes.cs
+++ api/v1/Routes/TodoRoutes/TodoRoutes.cs
@@ -1,7 +1,41 @@
+using api.v1.Models;
+using api.v1.Services;
 namespace api.v1.Routes;
+
 
 public static class TodoRoutes{
     public static void MapTodoRoutes(this IEndpointRouteBuilder app){
+        RouteGroupBuilder todoRoutes = app.MapGroup("/api/v1/todos");
 
+        todoRoutes.MapGet("/get-todos", (ITodoService service) => Results.Ok(service.GetTodos()));
+        todoRoutes.MapGet("/get-completed-todos", (ITodoService service) => {
+            var todosResult = service.GetCompletedTodos();
+
+            if (todosResult.Data == null){
+                return Results.NotFound(todosResult);
+            }
+            
+            return Results.Ok(todosResult.Data);
+        });
+
+        todoRoutes.MapGet("/get-incompleted-todos", (ITodoService service) => {
+            var todosResult = service.GetIncompletedTodos();
+
+            if (todosResult.Data == null){
+                return Results.NotFound(todosResult);
+            }
+
+            return Results.Ok(todosResult.Data);
+        });
+
+        todoRoutes.MapGet("/get-todo/{id}", (ITodoService service, int id) => {
+            ServiceResult<Todo> getResult = service.GetTodoById(id);
+        
+            if (getResult.Data == null){
+                return Results.NotFound(getResult);
+            }
+            
+            return Results.Ok(service.GetTodoById(id).Data);
+        });
     }
 }
