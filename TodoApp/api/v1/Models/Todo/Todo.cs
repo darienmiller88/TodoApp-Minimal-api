2913ce45a8970c352e36847269830404e3bad46f
unknown <darienmiller88@yahoo.com>
2025-05-05 15:57:23 -0400

    Re-added TodoApp as regular folder

---
+++ TodoApp/api/v1/Models/Todo/Todo.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.v1.Models;
+
+public class Todo{
+
+    [StringLength(100, MinimumLength = 5)]
+    public string todoName { get; set; }
+    public int id { get; set; }
+    public bool isComplete { get; set; }
+
+    public Todo(){
+        todoName = "";
+        id = 0;
+        isComplete = false;
+    }
+    
+    public Todo(string todoName, int id){
+        this.todoName = todoName;
+        this.id = id;
+        isComplete = false;
+    }
+
+    public override string ToString(){
+        return $"todo name: {todoName}\n id: {id}\n isComplete: {isComplete}";
+    }
+}
