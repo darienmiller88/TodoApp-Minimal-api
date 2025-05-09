a8747cbefd1d9c882f184578fd0fc9690a39a0b6
unknown <darienmiller88@yahoo.com>
2025-05-09 14:38:43 -0400

    Tests: added tests for GetIncompletedTodosHandler(), both for checking if it returns all incomplete todos, and if it returns a 404 for when there are no incomplete todos.

--- Tests/TestRouteHandlers/TestRouteHandlers.cs
+++ Tests/TestRouteHandlers/TestRouteHandlers.cs
@@ -50,4 +50,38 @@ public class TestTodoRouteHandlers{
 
         Assert.IsType<NotFound<ServiceResult<List<Todo>>>>(result);
     }
+
+    [Fact]
+    //Test to see if the GetIncompletedTodosHandler() returns all todos that are incomplete. 
+    //EXPECTED: List of todos containing Todos that are incompleted, with HttpResults type (ok).
+    public void TestGetIncompletedTodosHandler_WithAllIncomplete(){
+        ITodoService service = new TodoService(new List<Todo>{
+            new Todo("todo 1", 1, false),
+            new Todo("todo 2", 2, true),
+            new Todo("todo 3", 3, false),
+        });
+        var result = TodoRoutes.GetIncompletedTodosHandler(service);
+
+        Assert.IsType<Ok<List<Todo>>>(result);
+
+        var okResult = result as Ok<List<Todo>>;
+
+        Assert.NotNull(okResult);
+        Assert.NotNull(okResult.Value);
+        Assert.Equal(2, okResult.Value.Count);
+    }
+
+    [Fact]
+    //Test to see if the GetIncompletedTodosHandler() returns a 404 when all todos that are completed. 
+    //EXPECTED: Null, with a HttpResults type (NotFound).
+    public void TestGetIncompletedTodosHandler_WithAllComplete(){
+        ITodoService service = new TodoService(new List<Todo>{
+            new Todo("todo 1", 1, false),
+            new Todo("todo 2", 2, false),
+            new Todo("todo 3", 3, false),
+        });
+        var result = TodoRoutes.GetCompletedTodosHandler(service);
+
+        Assert.IsType<NotFound<ServiceResult<List<Todo>>>>(result);
+    }
 }
