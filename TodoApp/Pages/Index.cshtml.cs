c91e176c81dde5bf2875c08c0c038395c53e7b7d
unknown <darienmiller88@yahoo.com>
2025-05-11 12:00:43 -0400

    feat: injected c# code into razor page; added todos to Index model, retireves them from TodoService on get

--- TodoApp/Pages/Index.cshtml.cs
+++ TodoApp/Pages/Index.cshtml.cs
@@ -6,18 +6,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using api.v1.Services;
+using api.v1.Models;
 
 namespace TodoApp.Pages;
 public class IndexModel : PageModel{
     private readonly ILogger<Index> _logger;
     private readonly ITodoService _service;
+    public List<Todo> todos; 
 
     public IndexModel(ILogger<Index> logger, ITodoService service){
         _logger = logger;
         _service = service;
+        todos = [];
     }
 
     public void OnGet(){
+        todos = _service.GetTodos();
         
+        foreach (Todo todo in todos){
+            Console.WriteLine(todo);
+        }
+
     }
 }
