43ea8dea9b2cd2f5e2616103880ccf4bb8aeee46
unknown <darienmiller88@yahoo.com>
2025-05-14 11:14:18 -0400

    fix: Test todo route syntax errors

--- Tests/TestRouteHandlers/TestRouteHandlers.cs
+++ Tests/TestRouteHandlers/TestRouteHandlers.cs
@@ -6,13 +6,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using System.Collections.Generic;
 using api.v1.Models;
+using System.Threading.Tasks;
 
 public class TestTodoRouteHandlers{
     
     [Fact]
-    public void TestGetTodosHandler(){
+    public async Task TestGetTodosHandler(){
         ITodoService service = new TodoService();
-        IResult result = TodoRoutes.GetTodosHandler(service);
+        IResult result = await TodoRoutes.GetTodosHandler(service);
 
         //Ensures that the GetTodosHandler returns a list of todos.
         Assert.IsType<Ok<List<Todo>>>(result);
@@ -21,13 +22,9 @@ public class TestTodoRouteHandlers{
     [Fact]
     //Test to see if the GetCompletedTodosHandler() returns all todos that are completed. 
     //EXPECTED: List of todos containing Todos that are completed.
-    public void TestGetCompletedTodosHandler_WithAllComplete(){
-        ITodoService service = new TodoService(new List<Todo>{
-            new Todo("todo 1", 1, true),
-            new Todo("todo 2", 2, true),
-            new Todo("todo 3", 3, false),
-        });
-        var result = TodoRoutes.GetCompletedTodosHandler(service);
+    public async Task TestGetCompletedTodosHandler_WithAllComplete(){
+        ITodoService service = new TodoService();
+        var result = await TodoRoutes.GetCompletedTodosHandler(service);
 
         Assert.IsType<Ok<List<Todo>>>(result);
 
@@ -39,13 +36,9 @@ public class TestTodoRouteHandlers{
     }
 
      [Fact]
-    public void TestGetCompletedTodosHandler_WithAllIncomplete(){
-        ITodoService service = new TodoService(new List<Todo>{
-            new Todo("todo 1", 1, false),
-            new Todo("todo 2", 2, false),
-            new Todo("todo 3", 3, false),
-        });
-        var result = TodoRoutes.GetCompletedTodosHandler(service);
+    public async Task TestGetCompletedTodosHandler_WithAllIncomplete(){
+        ITodoService service = new TodoService();
+        var result = await TodoRoutes.GetCompletedTodosHandler(service);
 
         Assert.IsType<NotFound<ServiceResult<List<Todo>>>>(result);
     }
@@ -53,13 +46,9 @@ public class TestTodoRouteHandlers{
     [Fact]
     //Test to see if the GetIncompletedTodosHandler() returns all todos that are incomplete. 
     //EXPECTED: List of todos containing Todos that are incompleted, with HttpResults type (ok).
-    public void TestGetIncompletedTodosHandler_WithAllIncomplete(){
-        ITodoService service = new TodoService(new List<Todo>{
-            new Todo("todo 1", 1, false),
-            new Todo("todo 2", 2, true),
-            new Todo("todo 3", 3, false),
-        });
-        var result = TodoRoutes.GetIncompletedTodosHandler(service);
+    public async Task TestGetIncompletedTodosHandler_WithAllIncomplete(){
+        ITodoService service = new TodoService();
+        var result = await TodoRoutes.GetIncompletedTodosHandler(service);
 
         Assert.IsType<Ok<List<Todo>>>(result);
 
@@ -73,13 +62,9 @@ public class TestTodoRouteHandlers{
     [Fact]
     //Test to see if the GetIncompletedTodosHandler() returns a 404 when all todos that are completed. 
     //EXPECTED: Null, with a HttpResults type (NotFound).
-    public void TestGetIncompletedTodosHandler_WithAllComplete(){
-        ITodoService service = new TodoService(new List<Todo>{
-            new Todo("todo 1", 1, false),
-            new Todo("todo 2", 2, false),
-            new Todo("todo 3", 3, false),
-        });
-        var result = TodoRoutes.GetCompletedTodosHandler(service);
+    public async Task TestGetIncompletedTodosHandler_WithAllComplete(){
+        ITodoService service = new TodoService();
+        var result = await TodoRoutes.GetCompletedTodosHandler(service);
 
         Assert.IsType<NotFound<ServiceResult<List<Todo>>>>(result);
     }
@@ -87,13 +72,9 @@ public class TestTodoRouteHandlers{
     [Fact]
     //Test to see if the GetTodoByIdHandler() returns a valid todo with the right id.
     //EXPECTED: valid todo, Ok<> response with a ServiceResult<Todo>
-    public void TestGetTodoByIdHandler_WithValidId(){
-        ITodoService service = new TodoService(new List<Todo>{
-            new Todo("todo 1", 1, false),
-            new Todo("todo 2", 2, false),
-            new Todo("todo 3", 3, false),
-        });
-        var result = TodoRoutes.GetTodoByIdHandler(service, 2);
+    public async Task TestGetTodoByIdHandler_WithValidId(){
+        ITodoService service = new TodoService();
+        var result = await TodoRoutes.GetTodoByIdHandler(service, "");
 
         //This is what should be return by the handler
         Assert.IsType<Ok<Todo>>(result);
@@ -110,19 +91,15 @@ public class TestTodoRouteHandlers{
         Assert.NotNull(okResult.Value);
 
         //Afterwards, enure that the retrieved todo is the same as the one in the list.
-        Assert.Equal(new Todo("todo 2", 2, false), okResult.Value);
+        Assert.Equal(new Todo("todo 2", false), okResult.Value);
     }
 
     [Fact]
     //Test to see if the GetTodoByIdHandler() returns a NotFound response iwth null data todo.
     //EXPECTED: NotFound<> response with null
-    public void TestGetTodoByIdHandler_WithInvalidId(){
-        ITodoService service = new TodoService(new List<Todo>{
-            new Todo("todo 1", 1, false),
-            new Todo("todo 2", 2, false),
-            new Todo("todo 3", 3, false),
-        });
-        var result = TodoRoutes.GetTodoByIdHandler(service, 22);
+    public async Task TestGetTodoByIdHandler_WithInvalidId(){
+        ITodoService service = new TodoService();
+        var result = await TodoRoutes.GetTodoByIdHandler(service, "22");
 
         //This is what should be return by the handler
         Assert.IsType<NotFound<ServiceResult<Todo>>>(result);
@@ -131,11 +108,11 @@ public class TestTodoRouteHandlers{
     [Fact]
     //Test to see if the AddTodoHandler() returns a ok response after getting a valid todo.
     //EXPECTED: ok<> response with ServiceResult
-    public void TestAddTodoByIdHandler_WithValidTodo(){
+    public async Task TestAddTodoByIdHandler_WithValidTodo(){
         ITodoService service = new TodoService();
-        Todo todo = new Todo("todo to add", 22, false);
+        Todo todo = new Todo("todo to add", false);
         HttpContext context = new DefaultHttpContext();
-        var result = TodoRoutes.AddTodoHandler(service, todo, context);
+        var result = await TodoRoutes.AddTodoHandler(service, todo, context);
 
         //This is what should be return by the handler
         Assert.IsType<Created<ServiceResult<Todo>>>(result);
