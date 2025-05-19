f0e7d80d80b0df5d338bba4f56ae6b9efd8d2131
unknown <darienmiller88@yahoo.com>
2025-05-19 12:26:25 -0400

    fix: forms are able to call specific handlers in Index.cshmtl.cs now!

--- TodoApp/Pages/Index.cshtml.cs
+++ TodoApp/Pages/Index.cshtml.cs
@@ -9,27 +9,28 @@ using api.v1.Services;
 using api.v1.Models;
 
 namespace TodoApp.Pages;
-public class IndexModel : PageModel{
+
+public class IndexModel : PageModel {
     private readonly ILogger<IndexModel> _logger;
     private readonly ITodoService _service;
-    public List<Todo> todos; 
+    public List<Todo> todos;
 
     [BindProperty]
     //Forms from front end will bind to this object
     public Todo NewTodo { get; set; } = new();
 
-    public IndexModel(ILogger<IndexModel> logger, ITodoService service){
+    public IndexModel(ILogger<IndexModel> logger, ITodoService service) {
         _logger = logger;
         _service = service;
         todos = [];
     }
 
-    public async Task OnGet(){
+    public async Task OnGet() {
         todos = await _service.GetTodosAsync();
     }
 
-    public async Task<IActionResult> OnPost(){
-        if (!ModelState.IsValid){
+    public async Task<IActionResult> OnPost() {
+        if (!ModelState.IsValid) {
             return Page();
         }
 
@@ -39,7 +40,12 @@ public class IndexModel : PageModel{
         //And afterwards, refresh the list of Todos by retrieving the updated list from the database.
         todos = await _service.GetTodosAsync();
 
-        return RedirectToPage(); // Redirect to GET
+        return RedirectToPage(); // Redirect back to the page.
     }
 
+    public async Task<IActionResult> OnPostDeleteAsync(string id) {
+        Console.WriteLine("Delete called! id: " + id);
+
+        return RedirectToPage(); // Redirect to GET
+    }
 }
